231b48c1e0c06cf48283c441df5de9b1e7a9efb8
Peter <pipsrx@gmail.com>
2023-08-22 06:57:48 -0700

    Modified 1

--- Crashblog/blog/models.py
+++ Crashblog/blog/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ("title", )
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     intro = models.TextField()
@@ -10,3 +23,14 @@ class Post(models.Model):
 
     class Meta:
         ordering = ("-created_at",)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- Crashblog/blog/views.py
+++ Crashblog/blog/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+
+from blog.models import Post, Category
+from .forms import CommentForm
+
 
 # Create your views here.
+def detail(request, category_slug, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', slug=slug)
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/detail.html', {'post': post, 'form': form})
+
+
+def category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    return render(request, 'blog/category.html', {'category': category})
